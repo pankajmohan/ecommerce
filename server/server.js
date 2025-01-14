b6adce4ea14727a8d01f9f631e48cd83bdc5aefa
Pankaj Mohan <pankaj.mohan282@gmail.com>
2025-01-15 00:11:23 +0530

    updated server.js

--- server/server.js
+++ server/server.js
@@ -15,7 +15,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(cors({ origin: 'https://ecommerce-ic6h71vpo-pankajmohans-projects.vercel.app/ecommerce' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
